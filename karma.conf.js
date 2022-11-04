686587e9eaecf08786894784ad0415a514fc4d3e
Tim Konick <konick781>
2022-11-04 21:59:32 +0000

    Remove old files

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/web-app'),
+      dir: require('path').join(__dirname, './coverage/angular.io-example'),
       subdir: '.',
       reporters: [
         { type: 'html' },
