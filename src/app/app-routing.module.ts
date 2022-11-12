3db6f22b74cd5f647c00856996d7081a1887c9ee
Tim Konick <konick781>
2022-11-12 23:08:16 +0000

    Safe-Space

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MyWidgetComponent } from './my-widget/my-widget.component';
+import { ShowSadComponent } from './show-sad/show-sad.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: HeroDetailComponent },
   { path: 'heroes', component: HeroesComponent },
-  { path: 'my-widget', component: MyWidgetComponent}
+  { path: 'my-widget', component: MyWidgetComponent},
+  { path: 'show-sad', component: ShowSadComponent}
 ];
 
 @NgModule({
