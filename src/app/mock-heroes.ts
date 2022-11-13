5adea35bb6c8dfe5652ac10a429389032cc7fd83
Tim Konick <konick781>
2022-11-13 01:30:46 +0000

    Emotion mock test

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,13 +1,13 @@
 import { Hero } from './hero';
-
+// Don't judge the emotions lolz
 export const HEROES: Hero[] = [
-  { id: 12, name: 'Dr. Nice' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr. IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 12, name: 'Dr. Nice', emotion: 'sad' },
+  { id: 13, name: 'Bombasto', emotion: 'mad' },
+  { id: 14, name: 'Celeritas', emotion: 'glad' },
+  { id: 15, name: 'Magneta', emotion: 'confused' },
+  { id: 16, name: 'RubberMan', emotion: 'confident' },
+  { id: 17, name: 'Dynama', emotion: 'sleepy' },
+  { id: 18, name: 'Dr. IQ', emotion: 'determined' },
+  { id: 19, name: 'Magma', emotion: 'lost' },
+  { id: 20, name: 'Tornado', emotion: 'bewildering' }
 ];
