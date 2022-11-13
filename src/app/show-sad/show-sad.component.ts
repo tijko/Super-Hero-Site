d8f485fedc57dd3ce1462e09c9f270a90ec98e5c
Tim Konick <konick781>
2022-11-13 19:05:02 +0000

    Remove unused imports

--- src/app/show-sad/show-sad.component.ts
+++ src/app/show-sad/show-sad.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
 
 import { Hero } from '../hero';
-import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 import { HeroesComponent } from '../heroes/heroes.component';
-import { DashboardComponent } from '../dashboard/dashboard.component';
+
 
 @Component({
-  providers:[HeroesComponent, HttpClientModule],
+  providers:[HeroesComponent], 
   selector: 'app-show-sad',
   templateUrl: './show-sad.component.html',
   styleUrls: ['./show-sad.component.css']
